281d8f0176dde541b4d6fc2fb53d6e5d99b36762
jonathanctba19-png <jonathan.ctba19@gmail.com>
2025-10-03 01:02:30 -0300

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -17,7 +17,7 @@ CONFIG_FILE = "info_channels.json"
 class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_url = "http://raw.thug4ff.com/info"
+        self.api_url = "https://free-freefire-info-bot-d66l.onrender.com"
         self.generate_url = "http://profile.thug4ff.com/api/profile"
         self.session = aiohttp.ClientSession()
         self.config_data = self.load_config()
